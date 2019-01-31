54626b6806a7066c203421be8efe39a18e092fa0
Carl Bednorz <carl@studiofrnkn.com>
2019-01-31 11:13:36 +0100

    Relaunch

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+	"use strict";
+
+	var window_width 	 = $(window).width(),
+	window_height 		 = window.innerHeight,
+	header_height 		 = $(".default-header").height(),
+	header_height_static = $(".site-header.static").outerHeight(),
+	fitscreen 			 = window_height - header_height;
+
+
+	$(".fullscreen").css("height", window_height)
+	$(".fitscreen").css("height", fitscreen);
+
+     
+     // -------   Active Mobile Menu-----//
+
+    $(".menu-bar").on('click', function(e){
+        e.preventDefault();
+        $("nav").toggleClass('hide');
+        $("span", this).toggleClass("lnr-menu lnr-cross");
+        $(".main-menu").addClass('mobile-menu');
+    });
+     
+    
+
+    //  Counter Js 
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+ });
